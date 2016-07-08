4c46f1449dc84ba18ea85489adbb22614fba7bc7
Dynamics CRM Tools <dynamicscrmtools@gmail.com>
2016-07-08 08:11:06 +0200

    Unit test project added. Test implemented.

---
+++ AppVeyorEvaluation/AppVeyorEvaluation.Tests/UnitTest1.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace AppVeyorEvaluation.Tests {
+  [TestClass]
+  public class UnitTest1 {
+    [TestMethod]
+    public void TestMethod1() {
+      Class1 class1 = new Class1();
+
+      Assert.IsNull(class1.Name);
+    }
+  }
+}
